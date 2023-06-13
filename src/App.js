a7b5698c33f51be01d2074f75fce644676633ad9
aradhya <singh.aradhya2019@gmail.com>
2023-06-13 17:39:36 +0530

    styled form and card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import AddUser from "./components/Users/AddUsers";
-import Card from "./components/UI/Card"
+
 function App() {
   return (
-    <Card>
+
     <AddUser/>
-    </Card>
   );
 }
 
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,10 +1,13 @@
-const AddUser = (props) => {
+import classes from "./AddUsers.module.css"
+import Card from "../UI/Card"
 
+const AddUser = (props) => {
     const submitHandler = (event) => {
         event.preventDefault()
     }
     
     return(
+        <Card cardClasses={classes.form}>
         <form onSubmit={submitHandler}>
             <label htmlFor="name">name</label>
             <input id="name" type="text"/>
@@ -12,6 +15,7 @@ const AddUser = (props) => {
             <input id="age" type="number"/>
             <button type="submit">Add User</button>
         </form>
+        </Card>
     )
 }
 
